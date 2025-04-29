95404e284bf77f352db5ec002b5e51336abe9b2b
tetra_te <node4853@gmail.com>
2025-04-29 12:04:23 +0900

    Initial commit

---
+++ FitRotateEffect/FitRotateEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace FitRotateEffect
+{
+    [VideoEffect("フィット回転", ["描画"], ["fit rotate"], IsAviUtlSupported = false)]
+    class FitRotateEffect : VideoEffectBase
+    {
+        public override string Label => $"フィット回転 {Rotate.GetValue(0, 1, 30):F1}°";
+
+        [Display(GroupName = "フィット回転", Name = "回転角", Description = "回転させる角度（右回り）")]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation Rotate { get; } = new Animation(0, -36000, 36000);
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return [];
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new FitRotateEffectProcessor(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Rotate];
+    }
+}
---
+++ FitRotateEffect/FitRotateEffectProcessor.cs
@@ -0,0 +1,74 @@
+﻿using Vortice.Direct2D1;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+
+namespace FitRotateEffect
+{
+    class FitRotateEffectProcessor : IVideoEffectProcessor
+    {
+        readonly IGraphicsDevicesAndContext devices;
+
+        readonly FitRotateEffect item;  
+
+        ID2D1Image? input;
+
+        public ID2D1Image Output => input ?? throw new NullReferenceException(nameof(input) + " is null");
+
+        public FitRotateEffectProcessor(IGraphicsDevicesAndContext devices, FitRotateEffect item)
+        {
+            this.devices = devices;
+            this.item = item;
+        }
+
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var drawDesc = effectDescription.DrawDescription;
+
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            var rotate = item.Rotate.GetValue(frame, length, fps);
+            var rotateRad = rotate * Math.PI / 180;
+
+            var inputBounds = devices.DeviceContext.GetImageLocalBounds(input);
+            var width = inputBounds.Right - inputBounds.Left;
+            var height = inputBounds.Bottom - inputBounds.Top;
+
+            if (width == 0 || height == 0)
+                return drawDesc;
+
+            var cos = Math.Abs(Math.Cos(rotateRad));
+            var sin = Math.Abs(Math.Sin(rotateRad));
+
+            var rotatedWidth = width * cos + height * sin;
+            var rotatedHeight = width * sin + height * cos;
+
+            var scale = Math.Max(rotatedWidth / width, rotatedHeight / height);
+
+            return
+                drawDesc with
+                {
+                    Rotation = new(
+                        drawDesc.Rotation.X,
+                        drawDesc.Rotation.Y,
+                        drawDesc.Rotation.Z + (float)rotate),
+                    Zoom = drawDesc.Zoom * (float)scale
+                };
+        }
+
+        public void SetInput(ID2D1Image? input)
+        {
+            this.input = input;
+        }
+
+        public void ClearInput()
+        {
+            input = null;
+        }
+
+        public void Dispose()
+        {
+        }     
+    }
+}
